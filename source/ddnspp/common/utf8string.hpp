dc2214598dcfd8b2753309141407ffa3e29b3c72
John-Chan <cpp.cheen@gmail.com>
2014-11-25 16:36:30 +0800

    remove utf8string

--- source/ddnspp/common/utf8string.hpp
+++ source/ddnspp/common/utf8string.hpp
@@ -1,10 +1,10 @@
 ﻿#ifndef UTF8STRING_H
 #define UTF8STRING_H
 
-#include <muradin/string_helper/string_u8.hpp>
+//#include <muradin/string_helper/string_u8.hpp>
 
 
 namespace air{namespace common {
-	typedef muradin::string_helper::UTF8String	UTF8Str;
+	//typedef muradin::string_helper::UTF8String	UTF8Str;
 }}
 #endif // UTF8STRING_H
